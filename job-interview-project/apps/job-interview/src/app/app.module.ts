ef508a1f48ed88a2accd135f604c0c511afc3640
Nikola <nsretkovic@elfak.rs>
2021-07-04 23:51:49 +0200

    imported material lib in app module

--- job-interview-project/apps/job-interview/src/app/app.module.ts
+++ job-interview-project/apps/job-interview/src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatSliderModule} from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import {MaterialModule} from 'libs/material/src/lib/material.module'
+
+import { JobComponent } from './components/job/job.component';
+import { JobListComponent } from './components/job-list/job-list.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, HeaderComponent, JobComponent, JobListComponent],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
+  
+
 })
 export class AppModule {}
