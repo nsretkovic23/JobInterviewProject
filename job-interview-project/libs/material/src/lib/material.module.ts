0be595bb3ac7c9b248729935a81a9f17b63f190b
Nikola <nsretkovic@elfak.rs>
2021-07-05 20:22:27 +0200

    minor styling changes in header

--- job-interview-project/libs/material/src/lib/material.module.ts
+++ job-interview-project/libs/material/src/lib/material.module.ts
@@ -3,18 +3,24 @@ import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   imports: [
     CommonModule, 
     MatButtonModule,
     MatSliderModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatInputModule
   ],
   exports:[
     MatButtonModule,
     MatSliderModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatInputModule
   ]
 })
 export class MaterialModule {}
