19a391b8b889e4815f420ebf71395fd0e2e6ea00
Nikola <nsretkovic@elfak.rs>
2021-07-05 20:22:50 +0200

    added filter card with quick-filter buttons on main page

---
+++ job-interview-project/apps/job-interview/src/app/components/filter-card/filter-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'filter-card-component',
+  templateUrl: './filter-card.component.html',
+  styleUrls: ['./filter-card.component.scss']
+})
+export class FilterCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
