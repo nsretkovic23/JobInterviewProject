9faeecfcb5f5a652c6c34ac02cb28d1786b1dab5
Nikola <nsretkovic@elfak.rs>
2021-07-05 20:21:48 +0200

    job cards are now in job-list component

---
+++ job-interview-project/apps/job-interview/src/app/components/job-list/job-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'job-list-component',
+  templateUrl: './job-list.component.html',
+  styleUrls: ['./job-list.component.scss']
+})
+export class JobListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
