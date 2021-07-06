8e63ba9f3871d1aba1b31957618f79cfd0a9fdd0
Nikola <nsretkovic@elfak.rs>
2021-07-06 18:38:05 +0200

    initial job and test shared interfaces

--- job-interview-project/libs/api-interfaces/src/lib/api-interfaces.ts
+++ job-interview-project/libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,32 @@
 export interface Message {
   message: string;
 }
+
+export interface Job {
+    jobTitle:string,
+    jobPicture:string,
+    companyName:string,
+    description:string,
+    numberOfCandidates:number,
+    lookingFor:string[],
+    companyOffers:string[],
+    candidates:[{
+      username:string,
+      email:string,
+      grade:number
+    }],
+    testId:string
+}
+
+export interface Test {
+    jobId:string,
+    numberOfQuestions:number,
+    questions:[{
+      questionText:string,
+      questionImage:string,
+      answerOptions:[{
+        option:string
+      }],
+      rightAnswer:string,
+    }]
+}
