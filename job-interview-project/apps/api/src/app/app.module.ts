923cd96acc25a54bb8292cd53a61811e416d8e28
Nikola <nsretkovic@elfak.rs>
2021-07-06 18:53:08 +0200

    imported controllers into app module

--- job-interview-project/apps/api/src/app/app.module.ts
+++ job-interview-project/apps/api/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JobsController } from '../controllers/jobs/jobs.controller';
+import { TestsController } from '../controllers/tests/tests.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, JobsController, TestsController],
   providers: [AppService],
 })
 export class AppModule {}
