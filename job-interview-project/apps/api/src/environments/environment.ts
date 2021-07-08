a8b961f8fb1828436033e458ee9f0f5c92476f2e
Nikola <nsretkovic@elfak.rs>
2021-07-08 17:31:29 +0200

    added mongo uri connection string

--- job-interview-project/apps/api/src/environments/environment.ts
+++ job-interview-project/apps/api/src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: false,
+  mongoURI:'mongodb+srv://nsretkovic:nsretkovic@rwaangularprojekat.jzkvh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 };
