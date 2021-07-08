d5acc115a7f4de801b9a0c90ad7e60cb9a03b762
Nikola <nsretkovic@elfak.rs>
2021-07-08 17:30:55 +0200

    imported new modules

--- job-interview-project/apps/api/src/app/app.module.ts
+++ job-interview-project/apps/api/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { JobsController } from '../controllers/jobs/jobs.controller';
-import { TestsController } from '../controllers/tests/tests.controller';
+import {MongooseModule} from '@nestjs/mongoose';
+import { environment } from '../environments/environment';
+import { MongoClient } from 'mongodb';
+import { JobsModule } from '../modules/jobs.module';
+import { TestModule } from '../modules/tests.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, JobsController, TestsController],
+  imports: [JobsModule, TestModule, MongooseModule.forRoot(environment.mongoURI)],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
