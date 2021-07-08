a0d4ecb1367ed81f320959edb9b1321e04b81662
Nikola <nsretkovic@elfak.rs>
2021-07-08 17:29:30 +0200

    created models lib for sharing mongoose models/schemas

---
+++ job-interview-project/libs/models/src/lib/models.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import  {JobModel} from './jobModel/job.model';
+import  {TestModel}  from './testModel/test.model';
+
+@NgModule({
+  imports: [CommonModule],
+})
+export class ModelsModule {}
