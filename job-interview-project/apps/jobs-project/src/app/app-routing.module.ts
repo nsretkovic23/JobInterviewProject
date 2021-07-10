e6d3ce0e6828ec7269b1a3d371b27650e05e8c0d
Nikola <nsretkovic@elfak.rs>
2021-07-10 18:04:50 +0200

    added route to a specific job, enabled scroll restoration

--- job-interview-project/apps/jobs-project/src/app/app-routing.module.ts
+++ job-interview-project/apps/jobs-project/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {RouterModule, Routes} from '@angular/router';
 import { JobCatalogComponent } from './components/job-catalog/job-catalog.component';
 import { JobComponent } from './components/job/job.component';
 import { CreateJobComponent } from './components/create-job/create-job.component';
+import { JobDetailsComponent } from './components/job-details/job-details.component';
 
 const routes:Routes = [
   {
@@ -15,16 +16,21 @@ const routes:Routes = [
     component:CreateJobComponent
   },
   {
+    path:"job/:id",
+    component:JobDetailsComponent
+  },
+  {
     path:"**", //instead of 404 page redirect to home page
     redirectTo: ""
-  },
+  }
+  
 ]
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    [RouterModule.forRoot(routes)]
+    [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})]
   ],
   exports: [RouterModule]
 })
