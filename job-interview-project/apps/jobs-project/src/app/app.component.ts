41177bb7c4f9280d82ab2f8e9951f598ef4b9509
Nikola <nsretkovic@elfak.rs>
2021-07-10 18:05:51 +0200

    unnecessary comments deleted

--- job-interview-project/apps/jobs-project/src/app/app.component.ts
+++ job-interview-project/apps/jobs-project/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Job, Test, Message } from '@job-interview-project/api-interfaces';
 import { AppState } from './store/job/app.state';
 import { Store } from '@ngrx/store';
-import { loadJobs, loadJobsSuccess } from './store/job/jobs.actions';
+import { loadJobs } from './store/job/jobs.actions';
 
 @Component({
   selector: 'job-interview-project-root',
@@ -11,10 +10,11 @@ import { loadJobs, loadJobsSuccess } from './store/job/jobs.actions';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit{
-  //hello$ = this.http.get<Message>('/api/hello');
+
   constructor(private store:Store<AppState>) {}
 
+
   ngOnInit() {
-    this.store.dispatch(loadJobs())
+    this.store.dispatch(loadJobs());
   }
 }
