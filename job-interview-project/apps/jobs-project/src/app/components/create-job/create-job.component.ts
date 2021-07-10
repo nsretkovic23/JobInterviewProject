cb487fe62909e07f9d9c4e49b603fe747144fb06
Nikola <nsretkovic@elfak.rs>
2021-07-10 03:41:43 +0200

    create job component for creating jobs

---
+++ job-interview-project/apps/jobs-project/src/app/components/create-job/create-job.component.ts
@@ -0,0 +1,60 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Job } from '@job-interview-project/api-interfaces';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/job/app.state';
+import { createJob } from '../../store/job/jobs.actions';
+
+@Component({
+  selector: 'job-interview-project-create-job',
+  templateUrl: './create-job.component.html',
+  styleUrls: ['./create-job.component.scss']
+})
+export class CreateJobComponent implements OnInit {
+
+  @ViewChild('titleInput') titleInput : ElementRef | null = null;
+  @ViewChild('descriptionText') descriptionText : ElementRef | null = null;
+  @ViewChild('lookingForText') lookingForText : ElementRef | null = null;
+  @ViewChild('weOfferText') weOfferText : ElementRef | null = null;
+  @ViewChild('datePicker') datePicker : ElementRef | null = null;
+  @ViewChild('pictureUrlInput') pictureUrl : ElementRef | null = null;
+  @ViewChild('companyNameInput') companyNameInput : ElementRef | null = null;
+  @ViewChild('locationInput') locationInput :ElementRef | null = null;
+
+  
+  selectedSeniority:string | null =null;
+  selectedJobType:string | null = null;
+  
+  newJob : Job | null = null;
+
+  constructor(private store:Store<AppState>) { }
+
+  minDate = new Date(Date.now());
+
+  ngOnInit(): void {
+  }
+
+  saveJob(){
+    if(this.selectedSeniority && this.selectedJobType)
+    {
+        this.newJob = {
+        jobTitle:this.titleInput?.nativeElement.value,
+        jobType:this.selectedJobType,
+        seniority:this.selectedSeniority,
+        jobPicture:this.pictureUrl?.nativeElement.value,
+        companyName:this.companyNameInput?.nativeElement.value,
+        description:this.descriptionText?.nativeElement.value,
+        expiryDate:this.datePicker?.nativeElement.value,
+        lookingFor:this.lookingForText?.nativeElement.value,
+        companyOffers:this.weOfferText?.nativeElement.value,
+        location:this.locationInput?.nativeElement.value
+      }
+      
+      if(this.newJob)
+        this.store.dispatch(createJob({job:this.newJob}))
+
+      console.log(this.newJob);
+    }
+     
+  }
+
+}
