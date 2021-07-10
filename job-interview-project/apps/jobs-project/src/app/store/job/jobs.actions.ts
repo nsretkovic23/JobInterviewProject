9d4d8be06f8b6605374936c61dccaaf3b8b7c625
Nikola <nsretkovic@elfak.rs>
2021-07-10 03:36:23 +0200

    jobs actions for loading all jobs and creating jobs

---
+++ job-interview-project/apps/jobs-project/src/app/store/job/jobs.actions.ts
@@ -0,0 +1,16 @@
+import { createAction, props } from "@ngrx/store";
+import { Job } from "@job-interview-project/api-interfaces";
+
+export const loadJobs = createAction(
+    "Load jobs"
+);
+
+export const loadJobsSuccess = createAction(
+    "Load jobs Success",
+    props<{jobs:Job[]}>()
+)
+
+export const createJob = createAction(
+    "Create Job",
+    props<{job:Job}>()
+)
