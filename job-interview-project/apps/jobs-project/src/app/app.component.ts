2775768c2678a7ca9d7ffc70684784ec182c8f43
Nikola <nsretkovic@elfak.rs>
2021-07-10 03:43:03 +0200

    app components now loads jobs in store

--- job-interview-project/apps/jobs-project/src/app/app.component.ts
+++ job-interview-project/apps/jobs-project/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Job, Test, Message } from '@job-interview-project/api-interfaces';
+import { AppState } from './store/job/app.state';
+import { Store } from '@ngrx/store';
+import { loadJobs, loadJobsSuccess } from './store/job/jobs.actions';
 
 @Component({
   selector: 'job-interview-project-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+export class AppComponent implements OnInit{
+  //hello$ = this.http.get<Message>('/api/hello');
+  constructor(private store:Store<AppState>) {}
+
+  ngOnInit() {
+    this.store.dispatch(loadJobs())
+  }
 }
