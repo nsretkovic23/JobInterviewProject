1e6086096e3b3c7b78d2747aac7e4ff3a88fee9c
Nikola <nsretkovic@elfak.rs>
2021-07-10 03:38:56 +0200

    job list now loads movies

--- job-interview-project/apps/jobs-project/src/app/components/job-list/job-list.component.ts
+++ job-interview-project/apps/jobs-project/src/app/components/job-list/job-list.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, Output } from '@angular/core';
+import { Job } from '@job-interview-project/api-interfaces';
+import { Store } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { AppState } from '../../store/job/app.state';
+import { selectAllJobs } from '../../store/job/jobs.selector';
 
 @Component({
   selector: 'job-list-component',
   templateUrl: './job-list.component.html',
   styleUrls: ['./job-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class JobListComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  jobs:Observable<readonly Job[]> = of([]);
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.jobs = this.store.select(selectAllJobs);
+  } 
 }
