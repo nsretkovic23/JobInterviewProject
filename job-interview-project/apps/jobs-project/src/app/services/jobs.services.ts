917c155086cebea2704221d2f4e27a3284b553e1
Nikola <nsretkovic@elfak.rs>
2021-07-10 03:31:48 +0200

    made jos service with getAll, getById and post methods

---
+++ job-interview-project/apps/jobs-project/src/app/services/jobs.services.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Job } from '@job-interview-project/api-interfaces';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+    providedIn: 'root',
+  })
+
+export class JobsService {
+    
+    constructor(private httpClient:HttpClient){}
+
+    getAll(){
+        return this.httpClient
+            .get<Job[]>(`${environment.apiURL}/jobs`)
+            .pipe(catchError(errorHandler));
+    }
+
+    getById(id:string){
+        return this.httpClient
+            .get<Job[]>(`${environment.apiURL}/jobs/${id}`)
+            .pipe(catchError(errorHandler));
+    }
+
+    createJob(job:Job){
+      return this.httpClient
+            .post<Job>(`${environment.apiURL}/jobs`, job)
+            .pipe(catchError(errorHandler));
+    }
+}
+
+const errorHandler = (error: HttpErrorResponse) => {
+    const errorMessage =
+      error.status === 0
+        ? `Can't connect to API ${error.error}`
+        : `Backend returned code ${error.status}`;
+  
+    return throwError(errorMessage);
+  };
