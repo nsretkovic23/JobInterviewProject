ad52f04faec8d7e0224e95321c630335ffc2827f
Nikola <nsretkovic@elfak.rs>
2021-07-14 04:15:33 +0200

    filter, update and delete job actions added

--- job-interview-project/apps/jobs-project/src/app/store/job/jobs.actions.ts
+++ job-interview-project/apps/jobs-project/src/app/store/job/jobs.actions.ts
@@ -1,21 +1,22 @@
-import { createAction, props } from "@ngrx/store";
-import { Job } from "@job-interview-project/api-interfaces";
+import { createAction, props } from '@ngrx/store';
+import { Job } from '@job-interview-project/api-interfaces';
 
-export const loadJobs = createAction(
-    "Load jobs"
-);
+export const loadJobs = createAction('Load jobs');
 
 export const loadJobsSuccess = createAction(
-    "Load jobs Success",
-    props<{jobs:Job[]}>()
-)
+  'Load jobs Success',
+  props<{ jobs: Job[] }>()
+);
 
-export const createJob = createAction(
-    "Create Job",
-    props<{job:Job}>()
-)
+export const createJob = createAction('Create Job', props<{ job: Job }>());
 
-export const selectJob = createAction(
-    "Select Job",
-    props<{jobId:string}>()
-)
+export const selectJob = createAction('Select Job', props<{ jobId: string }>());
+
+export const filterJobs = createAction(
+  'Filter Jobs',
+  props<{ filterJobsString: string }>()
+);
+
+export const updateJob = createAction('Update Job', props<{ job: Job }>());
+
+export const deleteJob = createAction('Delete Job', props<{ id: string }>());
