9e53cc3c81159be294f0fd25c654d7d8c0cc95a8
Nikola <nsretkovic@elfak.rs>
2021-07-14 04:14:41 +0200

    job list now displays jobs by filter string

--- job-interview-project/apps/jobs-project/src/app/components/job-list/job-list.component.ts
+++ job-interview-project/apps/jobs-project/src/app/components/job-list/job-list.component.ts
@@ -1,23 +1,37 @@
-import { ChangeDetectionStrategy, Component, OnInit, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+} from '@angular/core';
 import { Job } from '@job-interview-project/api-interfaces';
 import { Store } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
 import { AppState } from '../../store/job/app.state';
-import { selectAllJobs } from '../../store/job/jobs.selector';
+import * as Selectors from '../../store/job/jobs.selector';
+import * as Actions from '../../store/job/jobs.actions';
 
 @Component({
   selector: 'job-list-component',
   templateUrl: './job-list.component.html',
   styleUrls: ['./job-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class JobListComponent implements OnInit {
+export class JobListComponent implements OnInit, OnChanges {
+  @Input() filterString = '';
 
-  jobs:Observable<readonly Job[]> = of([]);
+  jobs: Observable<readonly Job[]> = of([]);
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.jobs = this.store.select(selectAllJobs);
-  } 
+    this.jobs = this.store.select(Selectors.selectFilteredJobs);
+  }
+
+  ngOnChanges() {
+    this.store.dispatch(
+      Actions.filterJobs({ filterJobsString: this.filterString })
+    );
+  }
 }
