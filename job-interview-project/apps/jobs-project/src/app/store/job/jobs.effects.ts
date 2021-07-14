afdc9af02341df5277d30cddfbaa135611fa9ca1
Nikola <nsretkovic@elfak.rs>
2021-07-14 04:15:55 +0200

    save, update, and delete job effects added

--- job-interview-project/apps/jobs-project/src/app/store/job/jobs.effects.ts
+++ job-interview-project/apps/jobs-project/src/app/store/job/jobs.effects.ts
@@ -1,41 +1,71 @@
-import { Injectable } from "@angular/core";
-import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { of } from "rxjs";
-import { catchError, map, mergeMap, switchMap } from "rxjs/operators";
-import { JobsService } from "../../services/jobs.services";
-import { AppState } from "./app.state";
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, map, mergeMap, switchMap } from 'rxjs/operators';
+import { JobsService } from '../../services/jobs.services';
 import * as JobsActions from './jobs.actions';
 
 @Injectable()
 export class JobsEffects {
-    
-    constructor(private jobsService :JobsService, private action$ : Actions){}
+  constructor(private jobsService: JobsService, private action$: Actions) {}
 
-    loadEffect$ = createEffect(() =>
-        this.action$.pipe(
-            ofType(JobsActions.loadJobs),
-            mergeMap(() => this.jobsService.getAll()
-                .pipe(
-                    map((jobs) => (JobsActions.loadJobsSuccess({jobs}))),
-                    catchError(() => of({type:"loading jobs error"}))
-                )
+  loadEffect$ = createEffect(() =>
+    this.action$.pipe(
+      ofType(JobsActions.loadJobs),
+      mergeMap(() =>
+        this.jobsService.getAll().pipe(
+          map((jobs) => JobsActions.loadJobsSuccess({ jobs })),
+          catchError(() => of({ type: 'loading jobs error' }))
+        )
+      )
+    )
+  );
+
+  saveEffect$ = createEffect(() =>
+    this.action$.pipe(
+      ofType(JobsActions.createJob),
+      switchMap((action) =>
+        this.jobsService.createJob(action.job).pipe(
+          mergeMap(() =>
+            this.jobsService.getAll().pipe(
+              map((jobs) => JobsActions.loadJobsSuccess({ jobs })),
+              catchError(() => of({ type: 'loading jobs error' }))
             )
+          )
         )
+      )
     )
+  );
 
-    saveEffect$ = createEffect(() =>
-        this.action$.pipe(
-            ofType(JobsActions.createJob),
-            switchMap((action) => this.jobsService.createJob(action.job) 
-            .pipe(       
-                mergeMap(() => this.jobsService.getAll()
-                .pipe(
-                    map((jobs) => (JobsActions.loadJobsSuccess({jobs}))),
-                    catchError(() => of({type:"loading jobs error"}))
-                        )
-                    )
-                )
-            )        
+  updateEffect$ = createEffect(() =>
+    this.action$.pipe(
+      ofType(JobsActions.updateJob),
+      switchMap((action) =>
+        this.jobsService.updateJob(action.job).pipe(
+          mergeMap(() =>
+            this.jobsService.getAll().pipe(
+              map((jobs) => JobsActions.loadJobsSuccess({ jobs })),
+              catchError(() => of({ type: 'loading jobs error' }))
+            )
+          )
         )
+      )
     )
+  );
+
+  deleteEffect = createEffect(() =>
+    this.action$.pipe(
+      ofType(JobsActions.deleteJob),
+      switchMap((action) =>
+        this.jobsService.deleteJob(action.id).pipe(
+          mergeMap(() =>
+            this.jobsService.getAll().pipe(
+              map((jobs) => JobsActions.loadJobsSuccess({ jobs })),
+              catchError(() => of({ type: 'loading jobs error' }))
+            )
+          )
+        )
+      )
+    )
+  );
 }
