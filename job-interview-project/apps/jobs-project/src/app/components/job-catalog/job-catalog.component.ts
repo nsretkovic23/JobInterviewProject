5d0bf627d396d752ab6edd1d248052d343e2a452
Nikola <nsretkovic@elfak.rs>
2021-07-14 04:12:30 +0200

    catching filter string and forwarding to job list comp

--- job-interview-project/apps/jobs-project/src/app/components/job-catalog/job-catalog.component.ts
+++ job-interview-project/apps/jobs-project/src/app/components/job-catalog/job-catalog.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { AppState } from '../../store/job/app.state';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'job-catalog-component',
   templateUrl: './job-catalog.component.html',
-  styleUrls: ['./job-catalog.component.scss']
+  styleUrls: ['./job-catalog.component.scss'],
 })
 export class JobCatalogComponent implements OnInit {
+  filter: string = 'All';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  filterJobs(filterString: string) {
+    this.filter = filterString;
   }
-
 }
