72e9081f61ed6290cf2a74652eb5767098ec73ba
Nikola <nsretkovic@elfak.rs>
2021-07-14 04:14:58 +0200

    update and delete api service added

--- job-interview-project/apps/jobs-project/src/app/services/jobs.services.ts
+++ job-interview-project/apps/jobs-project/src/app/services/jobs.services.ts
@@ -6,37 +6,47 @@ import { throwError } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({
-    providedIn: 'root',
-  })
-
+  providedIn: 'root',
+})
 export class JobsService {
-    
-    constructor(private httpClient:HttpClient){}
+  constructor(private httpClient: HttpClient) {}
 
-    getAll(){
-        return this.httpClient
-            .get<Job[]>(`${environment.apiURL}/jobs`)
-            .pipe(catchError(errorHandler));
-    }
+  getAll() {
+    return this.httpClient
+      .get<Job[]>(`${environment.apiURL}/jobs`)
+      .pipe(catchError(errorHandler));
+  }
 
-    getById(id:string){
-        return this.httpClient
-            .get<Job[]>(`${environment.apiURL}/jobs/${id}`)
-            .pipe(catchError(errorHandler));
-    }
+  getById(id: string) {
+    return this.httpClient
+      .get<Job[]>(`${environment.apiURL}/jobs/${id}`)
+      .pipe(catchError(errorHandler));
+  }
 
-    createJob(job:Job){
-      return this.httpClient
-            .post<Job>(`${environment.apiURL}/jobs`, job)
-            .pipe(catchError(errorHandler));
-    }
+  createJob(job: Job) {
+    return this.httpClient
+      .post<Job>(`${environment.apiURL}/jobs`, job)
+      .pipe(catchError(errorHandler));
+  }
+
+  updateJob(job: Job) {
+    return this.httpClient
+      .put<Job>(`${environment.apiURL}/jobs/${job._id}`, job)
+      .pipe(catchError(errorHandler));
+  }
+
+  deleteJob(id: string) {
+    return this.httpClient
+      .delete<Job>(`${environment.apiURL}/jobs/${id}`)
+      .pipe(catchError(errorHandler));
+  }
 }
 
 const errorHandler = (error: HttpErrorResponse) => {
-    const errorMessage =
-      error.status === 0
-        ? `Can't connect to API ${error.error}`
-        : `Backend returned code ${error.status}`;
-  
-    return throwError(errorMessage);
-  };
+  const errorMessage =
+    error.status === 0
+      ? `Can't connect to API ${error.error}`
+      : `Backend returned code ${error.status}`;
+
+  return throwError(errorMessage);
+};
