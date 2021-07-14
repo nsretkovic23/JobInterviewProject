50dffac25739b94168e92a282fb200a922ff8be7
Nikola <nsretkovic@elfak.rs>
2021-07-14 04:10:07 +0200

    final-formatting code

--- job-interview-project/apps/api/src/services/jobs/jobs.service.ts
+++ job-interview-project/apps/api/src/services/jobs/jobs.service.ts
@@ -1,35 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { Job } from '@job-interview-project/api-interfaces';
-import {Model} from 'mongoose';
+import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { from, Observable } from 'rxjs';
-import { JobModel } from 'libs/models/src/lib/jobModel/job.model';
 
 @Injectable()
 export class JobsService {
+  constructor(@InjectModel('Job') private readonly jobModel: Model<Job>) {}
 
-    constructor(@InjectModel('Job') private readonly jobModel:Model<Job>){}
+  findAll(): Observable<Job[]> {
+    return from(this.jobModel.find());
+  }
 
-    findAll() : Observable<Job[]> { //observable?
-        return from(this.jobModel.find());
-    }
+  async findById(id: string): Promise<Job> {
+    return await this.jobModel.findOne({ _id: id });
+  }
 
-    async findById(id:string): Promise<Job> {
-        console.log("proba")
-        return await this.jobModel.findOne({_id:id});
-    }
+  async create(job: Job): Promise<Job> {
+    const newJob = new this.jobModel(job);
 
-    async create(job:Job) :Promise<Job>{
-        const newJob = new this.jobModel(job);
+    return await newJob.save();
+  }
 
-        return await newJob.save();
-    }
+  async delete(id: string): Promise<Job> {
+    return await this.jobModel.findByIdAndDelete(id);
+  }
 
-    async delete(id:string) :Promise<Job>{
-       return await this.jobModel.findByIdAndDelete(id);
-    }
-
-    async update(job:Job, id:string) :Promise<Job> {
-        return await this.jobModel.findByIdAndUpdate(id, job, {new:true})
-    }
+  async update(job: Job, id: string): Promise<Job> {
+    return await this.jobModel.findByIdAndUpdate(id, job, { new: true });
+  }
 }
