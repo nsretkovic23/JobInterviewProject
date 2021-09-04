ca67a986390fee4d0ab8e9e7fe273cf61f8d176b
Nikola <nsretkovic@elfak.rs>
2021-09-04 02:02:13 +0200

    Create company signup component

---
+++ job-interview-project/apps/jobs-project/src/app/components/user-system/signup-company/signup-company.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'job-interview-project-signup-company',
+  templateUrl: './signup-company.component.html',
+  styleUrls: ['./signup-company.component.scss']
+})
+export class SignupCompanyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
