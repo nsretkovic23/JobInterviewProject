dc3b4a9b8e902249c99eb11f92f716e51ca432c3
Nikola <nsretkovic@elfak.rs>
2021-09-04 02:01:29 +0200

    Create user profile page component

---
+++ job-interview-project/apps/jobs-project/src/app/components/user-system/user-profile-page/user-profile-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'job-interview-project-user-profile-page',
+  templateUrl: './user-profile-page.component.html',
+  styleUrls: ['./user-profile-page.component.scss']
+})
+export class UserProfilePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
