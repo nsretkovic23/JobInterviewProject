f12e681e74835c5fedac41b3f15044a789854d67
Nikola <nsretkovic@elfak.rs>
2021-09-04 02:02:52 +0200

    Create company profile page component

---
+++ job-interview-project/apps/jobs-project/src/app/components/user-system/company-profile-page/company-profile-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CompanyProfilePageComponent } from './company-profile-page.component';
+
+describe('CompanyProfilePageComponent', () => {
+  let component: CompanyProfilePageComponent;
+  let fixture: ComponentFixture<CompanyProfilePageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CompanyProfilePageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CompanyProfilePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ job-interview-project/apps/jobs-project/src/app/components/user-system/company-profile-page/company-profile-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'job-interview-project-company-profile-page',
+  templateUrl: './company-profile-page.component.html',
+  styleUrls: ['./company-profile-page.component.scss']
+})
+export class CompanyProfilePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
