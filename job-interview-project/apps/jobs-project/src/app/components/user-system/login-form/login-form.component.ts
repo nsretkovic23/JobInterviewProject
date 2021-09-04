00827de2f1b7422a7d42ad8b7cd04007a27ab177
Nikola <nsretkovic@elfak.rs>
2021-09-04 02:02:39 +0200

    Create login form component

---
+++ job-interview-project/apps/jobs-project/src/app/components/user-system/login-form/login-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'job-interview-project-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
