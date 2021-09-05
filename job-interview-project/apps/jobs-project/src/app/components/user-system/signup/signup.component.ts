da41374bf125126571207336d0658a037eeca88e
Nikola <nsretkovic@elfak.rs>
2021-09-05 03:30:23 +0200

    Implement picking up form values logic for signing up

---
+++ job-interview-project/apps/jobs-project/src/app/components/user-system/signup/signup.component.ts
@@ -0,0 +1,67 @@
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { Component, NgZone, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { take } from 'rxjs/operators';
+
+@Component({
+  selector: 'job-interview-project-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  hidePassword = true;
+  @ViewChild('autosize') autosizeUserDesc: CdkTextareaAutosize | undefined;
+
+  constructor(private _ngZone: NgZone, private userFormBuilder:FormBuilder, private companyFormBuilder:FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  userSignupForm : FormGroup = this.userFormBuilder.group({
+    userFullName : new FormControl('', [Validators.required, Validators.minLength(3)]),
+    userEmail : new FormControl('', [Validators.required, Validators.email]),
+    userPassword : new FormControl('', [Validators.required, Validators.minLength(6)]),
+    // TODO: Change this so user can import his own picture instead of placing the link
+    userPicture : new FormControl('https://icon-library.com/images/account-icon-png/account-icon-png-2.jpg'),
+    userDescription : new FormControl('',[Validators.required])
+  })
+  
+  companySignupForm : FormGroup = this.companyFormBuilder.group({
+    companyFullName : new FormControl('', [Validators.required, Validators.minLength(3)]),
+    companyEmail : new FormControl('', [Validators.required, Validators.email]),
+    companyPassword : new FormControl('', [Validators.required, Validators.minLength(6)]),
+    // TODO: Change this so user can import his own picture instead of placing the link
+    companyPicture : new FormControl('https://img.favpng.com/4/24/8/computer-icons-building-planacy-ab-architectural-engineering-biurowiec-png-favpng-4tXAHFuj3fEV7na0R5XfKBBz3.jpg'),
+    companyDescription : new FormControl('',[Validators.required]),
+    companyLocation : new FormControl('', [Validators.required]),
+  })
+  
+
+  onUserSignUp(){
+    if(!this.userSignupForm.valid)
+    {
+      console.log("Invalid user inputs");
+      return;
+    }
+    console.log(this.userSignupForm.value);
+  }
+
+  onCompanySignUp(){
+    if(!this.companySignupForm.valid)
+    {
+      console.log("Invalid company inputs");
+      return;
+    }
+    console.log(this.companySignupForm.value);
+  }
+
+  // HACK: Solution found and copy-pasted for auto resizing textarea in AngMaterials docs
+  triggerResize() {
+    // Wait for changes to be applied, then trigger textarea resize.
+    this._ngZone.onStable.pipe(take(1))
+        .subscribe(() => {
+          if(this.autosizeUserDesc)
+          this.autosizeUserDesc.resizeToFitContent(true)});
+  }
+
+}
