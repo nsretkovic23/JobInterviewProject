6224a9db1c9772ef233b8622b21611d0f5218a2c
Nikola <nsretkovic@elfak.rs>
2021-09-08 02:48:57 +0200

    Tune login form submitting and add snackBar for showing additional info

--- job-interview-project/apps/jobs-project/src/app/components/user-system/login-form/login-form.component.ts
+++ job-interview-project/apps/jobs-project/src/app/components/user-system/login-form/login-form.component.ts
@@ -1,46 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
-
 export class LoginFormComponent implements OnInit {
-  hide=true;
+  hide = true;
   email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
 
-  constructor() {}
+  constructor(private loginSnackBar: MatSnackBar) {}
 
-  ngOnInit(): void {  
+  ngOnInit(): void {}
+
+  // TODO: Open one snack bar if there is invalid input and open one more if server checked inputs and there is no user
+  openSnackBar() {
+    this.loginSnackBar.open('Check Your Inputs!', 'OK', { duration: 2000 });
   }
 
   // TODO: Check for errors in one if statement and give "universal" answer for potential errors
-  onLogin(){
-    if(this.email.hasError('required')){
-      console.log("You must enter email");
-      return;
-    }
-    if(this.email.hasError('email')){
-      console.log("Invalid mail address");
-      return;
-    }
-    if(this.password.hasError('required'))
-    {
-      console.log("You must enter password");
-      this.password.setValue('');
-      return;
-    }
-    if(this.password.hasError('minlength'))
-    {
-      console.log(`Length must be greater than ${this.password.errors?.minlength?.requiredLength}`);
-      this.password.setValue('');
+  onLogin() {
+    if (
+      this.email.hasError('required') ||
+      this.email.hasError('email') ||
+      this.password.hasError('required') ||
+      this.password.hasError('minlength')
+    ) {
+      this.openSnackBar();
       return;
     }
     console.log(this.email.value);
     console.log(this.password.value);
   }
-
 }
