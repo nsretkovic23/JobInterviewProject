9ea9322cc2aa71c22bcbc6e1cc604ac999da3aa5
Nikola <nsretkovic@elfak.rs>
2021-09-09 08:12:50 +0200

    Implement needed auth methods

--- job-interview-project/apps/api/src/services/auth/auth.service.ts
+++ job-interview-project/apps/api/src/services/auth/auth.service.ts
@@ -3,11 +3,12 @@ import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcrypt';
 import { Company, User } from '@job-interview-project/api-interfaces';
 import { CompanyService } from '../company/company.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     // HACK: Could've been done with Injecting company/user mongoDB models and using DTO's as a function parameters
-    constructor(private userService:UserService, private companyService:CompanyService){}
+    constructor(private userService:UserService, private companyService:CompanyService, private jwtService:JwtService){}
 
     async userSignUp(user:User):Promise<User>{
         const hashedPassword = await bcrypt.hash(user.userPassword, 10);
@@ -15,7 +16,7 @@ export class AuthService {
         const newUser : User = {...user, userPassword:hashedPassword};
 
         try{
-            return await this.userService.create(newUser);
+            return this.userService.create(newUser);
         }
         catch(error){
             throw new Error("Error USER signing up!");
@@ -28,10 +29,39 @@ export class AuthService {
         const newCompany:Company = {...company, companyPassword:hashedPassword};
         
         try{
-            return await this.companyService.create(newCompany);
+            return this.companyService.create(newCompany);
         }
         catch(error){
             throw new Error("Error COMPANY signing up!");
         }
+    }
+
+    async validateUser(userEmail:string, userPassword:string){
+        const user = await this.userService.findByEmail(userEmail);
+
+        if(!user)
+            return null;
+        
+        const isValid = await bcrypt.compare(userPassword, user.userPassword);
+
+        if(isValid)
+            return user;
+
+        return null;
+    }
+
+    // Controller calls this after running through the guard
+    async userSignIn(user:User){
+        // TODO: Try adding isUser:true to the payload
+        const payload = {username:user.userEmail, sub: user._id};
+
+        return{
+            accessToken: this.jwtService.sign(payload),
+            user
+        };
+    }
+
+    async getLoggedInUser(id:string):Promise<User>{
+        return this.userService.findById(id);
     }
 }
