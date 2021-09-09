e6cdd804711c6e86149f1f5e9e4a4e4ec64cf698
Nikola <nsretkovic@elfak.rs>
2021-09-09 08:02:51 +0200

    Implement user profile logic to listen to the route

--- job-interview-project/apps/jobs-project/src/app/components/user-system/user-profile-page/user-profile-page.component.ts
+++ job-interview-project/apps/jobs-project/src/app/components/user-system/user-profile-page/user-profile-page.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '@job-interview-project/api-interfaces';
+import { Store } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { AppState } from '../../../store/job/app.state';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { selectSignedInUser, selectTemporaryUser } from '../../../store/user/user.selector';
+import { loadUserById } from '../../../store/user/user.actions';
 
 @Component({
   selector: 'job-interview-project-user-profile-page',
@@ -6,10 +13,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile-page.component.scss']
 })
 export class UserProfilePageComponent implements OnInit {
+  
+  userId:string|null = null;
+  userObs:Observable<User | null> = of(null);
+  user:User | null = null;
 
-  constructor() { }
+  
+  constructor(private store:Store<AppState>, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params:ParamMap) => {
+      this.userId = params.get('id');
+
+      if(this.userId)
+        this.store.dispatch(loadUserById({id:this.userId}))
+
+      this.userObs = this.store.select(selectTemporaryUser);
+
+      this.userObs.subscribe((user) => {
+        if(user) this.user=user;
+      })
+    })
   }
 
 }
