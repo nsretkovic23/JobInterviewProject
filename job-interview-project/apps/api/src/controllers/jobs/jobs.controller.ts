55839e469ef96c71439457440abb71d4d72343c7
Nikola <nsretkovic@elfak.rs>
2021-09-09 08:14:01 +0200

    Set jwt guard on appropriate methods

--- job-interview-project/apps/api/src/controllers/jobs/jobs.controller.ts
+++ job-interview-project/apps/api/src/controllers/jobs/jobs.controller.ts
@@ -7,8 +7,10 @@ import {
   Delete,
   Param,
   Body,
+  UseGuards,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { JwtAuthGuard } from '../../services/auth/guards/jwt-auth.guard';
 import { JobsService } from '../../services/jobs/jobs.service';
 
 @Controller('jobs')
@@ -25,16 +27,19 @@ export class JobsController {
     return this.jobsService.findById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() job: Job): Promise<Job> {
     return this.jobsService.create(job);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async delete(@Param('id') id): Promise<Job> {
     return this.jobsService.delete(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   async update(@Body() updatedJob: Job, @Param('id') id): Promise<Job> {
     return this.jobsService.update(updatedJob, id);
