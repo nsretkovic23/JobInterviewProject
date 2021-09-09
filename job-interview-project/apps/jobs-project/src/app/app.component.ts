e56411aace161cc282f27b891a2f925ac65b7832
Nikola <nsretkovic@elfak.rs>
2021-09-09 08:09:29 +0200

    Dispatch  loading signin user when page load, and signout when user sign out

--- job-interview-project/apps/jobs-project/src/app/app.component.ts
+++ job-interview-project/apps/jobs-project/src/app/app.component.ts
@@ -1,17 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { AppState } from './store/job/app.state';
 import { Store } from '@ngrx/store';
 import { loadJobs } from './store/job/jobs.actions';
+import { loadSignedInUser, signOutUser } from './store/user/user.actions';
+import { User } from '@job-interview-project/api-interfaces';
+import { Observable, of } from 'rxjs';
+import { selectSignedInUser } from './store/user/user.selector';
 
 @Component({
   selector: 'job-interview-project-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnChanges {
   constructor(private store: Store<AppState>) {}
+
+  signedInUser : Observable<User | null> | null = null;
 
   ngOnInit() {
     this.store.dispatch(loadJobs());
+    this.store.dispatch(loadSignedInUser());
+    this.signedInUser = this.store.select(selectSignedInUser);
+  }
+
+  ngOnChanges(){
+
+  }
+
+  signOut(){
+    this.store.dispatch(signOutUser());
+    this.signedInUser = null;
   }
 }
