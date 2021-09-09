dac55c82c6f2c9b24827d058261da539a64b9e0d
Nikola <nsretkovic@elfak.rs>
2021-09-09 08:14:48 +0200

    Set guards on appropriate methods

--- job-interview-project/apps/api/src/controllers/auth/auth.controller.ts
+++ job-interview-project/apps/api/src/controllers/auth/auth.controller.ts
@@ -6,9 +6,13 @@ import {
     Put,
     Delete,
     Param,
+    Request,
+    UseGuards,
     Body,
   } from '@nestjs/common';
 import { AuthService } from '../../services/auth/auth.service';
+import { JwtAuthGuard } from '../../services/auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from '../../services/auth/guards/local-auth.guard';
 import { UserService } from '../../services/user/user.service';
 
 
@@ -16,13 +20,27 @@ import { UserService } from '../../services/user/user.service';
 export class AuthController {
     constructor(private authService:AuthService){}
 
-    @Post('user')
+    @Post('user/signup')
     async userSignUp(@Body() user:User): Promise<User>{
         return this.authService.userSignUp(user);
     }
 
-    @Post('company')
+    @UseGuards(LocalAuthGuard)
+    @Post('user/signin')
+    async userSignIn(@Request() req){
+        return this.authService.userSignIn(req.user);
+    }
+
+    @Post('company/signup')
     async companySignUp(@Body() company:Company): Promise<Company>{
         return this.authService.companySignUp(company);
     }
+
+    // Test function - works
+    @UseGuards(JwtAuthGuard)
+    @Get('user/me')
+    async testUserGetMe(@Request() req):Promise<User>{
+        return this.authService.getLoggedInUser(req.user?._id);
+    }
+
 }
