5506d9a34e3fd56f30aa141de24c323e7708d699
Nikola <nsretkovic@elfak.rs>
2021-09-09 08:13:31 +0200

    Add JWT SECRET

--- job-interview-project/apps/api/src/environments/environment.ts
+++ job-interview-project/apps/api/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  mongoURI:'mongodb+srv://nsretkovic:nsretkovic@rwaangularprojekat.jzkvh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  mongoURI:'mongodb+srv://nsretkovic:nsretkovic@rwaangularprojekat.jzkvh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  JWT_SECRET: '123123123'
 };
